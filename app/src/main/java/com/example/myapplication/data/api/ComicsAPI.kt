0dcd40e2ee2bf306b00e722142cb6462cdef94b1
Kira Lebedev <kiral@spotify.com>
2023-04-27 11:17:42 +0200

    Making internet requests

--- app/src/main/java/com/example/myapplication/data/api/ComicsAPI.kt
+++ app/src/main/java/com/example/myapplication/data/api/ComicsAPI.kt
@@ -2,8 +2,13 @@ package com.example.myapplication.data.api
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ComicsAPI {
-    @GET("/v1/public/comics")
-    suspend fun getComics(): Response<ComicsAPIResponse>
+    @GET("comics")
+    suspend fun getComics(
+        @Query("ts") ts: String = Constant.ts,
+        @Query("hash") hash: String = Constant.hash(),
+        @Query("limit") limit: String = Constant.limit
+    ): Response<ComicsAPIResponse>
 }
--- app/src/main/java/com/example/myapplication/data/api/NetworkModule.kt
+++ app/src/main/java/com/example/myapplication/data/api/NetworkModule.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.data.api
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -10,6 +11,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class NetworkModule {
+
 
     private val moshi by lazy {
         val moshiBuilder = Moshi.Builder()
@@ -23,9 +25,19 @@ class NetworkModule {
         loggingInterceptor
     }
 
+    private fun addApiKeyToRequests(chain: Interceptor.Chain): okhttp3.Response {
+        val request = chain.request().newBuilder()
+        val originalHttpUrl = chain.request().url
+        val newUrl = originalHttpUrl.newBuilder()
+            .addQueryParameter("apikey", Constant.API_KEY).build()
+        request.url(newUrl)
+        return chain.proceed(request.build())
+    }
+
     private val httpClient by lazy {
         OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
+            .addInterceptor { chain -> return@addInterceptor addApiKeyToRequests(chain) }
             .build()
     }
 
